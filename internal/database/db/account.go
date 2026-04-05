697f9d4be8d790fb82ef8e59664040379fb67c53
Andy Wu <andywu.05@gmail.com>
2026-04-05 01:49:47 -0400

    feat: add profile-aware account operations

--- internal/database/db/account.go
+++ internal/database/db/account.go
@@ -57,3 +57,61 @@ func (db *Database) GetAnilistToken() string {
 	}
 	return acc.Token
 }
+
+func (db *Database) GetAccountByProfileID(profileID string) (*models.Account, error) {
+	var acc models.Account
+	err := db.gormdb.Where("profile_id = ?", profileID).First(&acc).Error
+	if err != nil {
+		return nil, err
+	}
+	return &acc, nil
+}
+
+func (db *Database) UpsertAccountForProfile(profileID string, username string, token string, viewer []byte) (*models.Account, error) {
+	var acc models.Account
+	err := db.gormdb.Where("profile_id = ?", profileID).First(&acc).Error
+
+	if err != nil {
+		acc = models.Account{
+			Username:  username,
+			Token:     token,
+			Viewer:    viewer,
+			ProfileID: profileID,
+		}
+		err = db.gormdb.Create(&acc).Error
+	} else {
+		acc.Username = username
+		acc.Token = token
+		acc.Viewer = viewer
+		err = db.gormdb.Save(&acc).Error
+	}
+
+	if err != nil {
+		db.Logger.Error().Err(err).Msg("db: Failed to upsert account for profile")
+		return nil, err
+	}
+
+	if accountCache != nil && accountCache.ID == acc.ID {
+		accountCache = &acc
+	}
+
+	return &acc, nil
+}
+
+func (db *Database) GetAnilistTokenForProfile(profileID string) string {
+	acc, err := db.GetAccountByProfileID(profileID)
+	if err != nil || acc.Token == "" {
+		return ""
+	}
+	return acc.Token
+}
+
+func (db *Database) ClearAccountForProfile(profileID string) error {
+	return db.gormdb.Model(&models.Account{}).
+		Where("profile_id = ?", profileID).
+		Updates(map[string]interface{}{
+			"username": "",
+			"token":    "",
+			"viewer":   nil,
+		}).Error
+}
