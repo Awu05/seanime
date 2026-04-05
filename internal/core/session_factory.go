c90d8b3af8d6fcfe75cf81c0b7b4cf3bf4d21370
Andy Wu <andywu.05@gmail.com>
2026-04-05 12:02:28 -0400

    feat: add session factory for per-profile streaming instances

---
+++ internal/core/session_factory.go
@@ -0,0 +1,99 @@
+package core
+
+import (
+	"seanime/internal/api/anilist"
+	"seanime/internal/directstream"
+	"seanime/internal/library/playbackmanager"
+	"seanime/internal/nativeplayer"
+	"seanime/internal/torrentstream"
+	"seanime/internal/videocore"
+	"time"
+)
+
+// CreateStreamSession creates a new ProfileStreamSession with all streaming components initialized.
+// This mirrors the initialization in initModulesOnce but creates per-profile instances.
+func (a *App) CreateStreamSession(profileID string) *ProfileStreamSession {
+	// Create VideoCore
+	vc := videocore.New(videocore.NewVideoCoreOptions{
+		WsEventManager:      a.WSEventManager,
+		Logger:              a.Logger,
+		ContinuityManager:   a.ContinuityManager,
+		MetadataProviderRef: a.MetadataProviderRef,
+		DiscordPresence:     a.DiscordPresence,
+		PlatformRef:         a.AnilistPlatformRef,
+		RefreshAnimeCollectionFunc: func() {
+			_, _ = a.RefreshAnimeCollection()
+		},
+		IsOfflineRef: a.IsOfflineRef(),
+	})
+
+	// Create NativePlayer (depends on VideoCore)
+	np := nativeplayer.New(nativeplayer.NewNativePlayerOptions{
+		WsEventManager: a.WSEventManager,
+		Logger:         a.Logger,
+		VideoCore:      vc,
+	})
+
+	// Create PlaybackManager
+	pm := playbackmanager.New(&playbackmanager.NewPlaybackManagerOptions{
+		WSEventManager:      a.WSEventManager,
+		Logger:              a.Logger,
+		PlatformRef:         a.AnilistPlatformRef,
+		MetadataProviderRef: a.MetadataProviderRef,
+		Database:            a.Database,
+		RefreshAnimeCollectionFunc: func() {
+			_, _ = a.RefreshAnimeCollection()
+		},
+		DiscordPresence:   a.DiscordPresence,
+		IsOfflineRef:      a.IsOfflineRef(),
+		ContinuityManager: a.ContinuityManager,
+	})
+
+	// Create DirectStreamManager (depends on NativePlayer + VideoCore)
+	dsm := directstream.NewManager(directstream.NewManagerOptions{
+		Logger:              a.Logger,
+		WSEventManager:      a.WSEventManager,
+		ContinuityManager:   a.ContinuityManager,
+		MetadataProviderRef: a.MetadataProviderRef,
+		DiscordPresence:     a.DiscordPresence,
+		PlatformRef:         a.AnilistPlatformRef,
+		RefreshAnimeCollectionFunc: func() {
+			_, _ = a.RefreshAnimeCollection()
+		},
+		IsOfflineRef: a.IsOfflineRef(),
+		NativePlayer: np,
+		VideoCore:    vc,
+		HMACTokenFunc: func(endpoint string, symbol string) string {
+			qp, err := a.GetServerPasswordHMACAuth().GenerateQueryParam(endpoint, symbol)
+			if err != nil {
+				return ""
+			}
+			return qp
+		},
+	})
+
+	// Create TorrentstreamRepository (depends on PlaybackManager, DirectStreamManager, NativePlayer)
+	tsr := torrentstream.NewRepository(&torrentstream.NewRepositoryOptions{
+		Logger:              a.Logger,
+		BaseAnimeCache:      anilist.NewBaseAnimeCache(),
+		CompleteAnimeCache:  anilist.NewCompleteAnimeCache(),
+		MetadataProviderRef: a.MetadataProviderRef,
+		TorrentRepository:   a.TorrentRepository,
+		PlatformRef:         a.AnilistPlatformRef,
+		PlaybackManager:     pm,
+		WSEventManager:      a.WSEventManager,
+		Database:            a.Database,
+		DirectStreamManager: dsm,
+		NativePlayer:        np,
+	})
+
+	return &ProfileStreamSession{
+		ProfileID:           profileID,
+		LastActive:          time.Now(),
+		VideoCore:           vc,
+		NativePlayer:        np,
+		PlaybackManager:     pm,
+		DirectStreamManager: dsm,
+		TorrentStream:       tsr,
+	}
+}
