d280f7c05e270a2ef67578c2baf262eb419df124
Andy Wu <andywu.05@gmail.com>
2026-04-12 12:45:31 -0400

    style: tighten filter row gap

--- seanime-web/src/app/(main)/schedule/_components/this-season-view.tsx
+++ seanime-web/src/app/(main)/schedule/_components/this-season-view.tsx
@@ -142,7 +142,7 @@ export function ThisSeasonView() {
                 </div>
 
                 {/* Filters row */}
-                <div className="flex items-center justify-center gap-3 pb-2">
+                <div className="flex items-center justify-center gap-2 pb-2">
                     <NativeSelect
                         value={formatFilter}
                         options={FORMAT_OPTIONS}
