cc128345751d2a900ea0f99b9ed3fa57bca9d679
Andy Wu <andywu.05@gmail.com>
2026-09-02 18:48:24 -0400

    chore: bump version to 4.3.17

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	Version              = "4.3.16"
+	Version              = "4.3.17"
 	VersionName          = "Hakumei"
 	GcTime               = time.Minute * 30
 	ConfigFileName       = "config.toml"
